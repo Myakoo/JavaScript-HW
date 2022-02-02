28767051226a9c9553f369967e85defa590d84c0
Myakoo <yourflexium@protonmail.com>
2022-02-02 16:23:50 +0600

    Fixed

--- index.js
+++
@@ -1,9 +0,0 @@
-function removeChar(string) {
-	string = string.slice(1,-1) // Интересно. Перебирал полчаса значение для последнего символа, добрёл кое-как до -1. Полез узнавать в чём подвох, а оказалось, что это как length-1 работает. Круто)
-	return string
-}
-
-
-console.log(removeChar('eloquent')); // loquen
-console.log(removeChar('country')); // ountr
-console.log(removeChar('person')); // erso
